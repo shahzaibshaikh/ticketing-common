101ff584a1d356bdc2901b4010fbefbbe371887b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-02-01 00:02:37 -0500

    Updates

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@ export * from "./errors/request-validation-error";
 export * from "./middlewares/current-user";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
+export * from "./middlewares/error-handler";
